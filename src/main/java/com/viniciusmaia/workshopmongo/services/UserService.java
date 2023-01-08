7729ad8f185787b40ce1e2d4eeb1eed766a573c8
Vinicius-Maia-A <vinicius.maia2011@gmail.com>
2023-01-08 18:38:50 -0300

    Obtendo um usuário por id

--- src/main/java/com/viniciusmaia/workshopmongo/services/UserService.java
+++ src/main/java/com/viniciusmaia/workshopmongo/services/UserService.java
@@ -1,10 +1,14 @@
 package com.viniciusmaia.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import com.viniciusmaia.workshopmongo.domain.User;
 import com.viniciusmaia.workshopmongo.repository.UserRepository;
+import com.viniciusmaia.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -15,4 +19,11 @@ public class UserService {
 	public List<User> findAll() {
 		return repo.findAll();
 	}
+	
+	public User findById(String id) {
+		
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!"));
+	}
+		
 }
