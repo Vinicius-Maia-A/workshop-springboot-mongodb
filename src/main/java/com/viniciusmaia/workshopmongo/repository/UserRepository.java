051ba18d0d0c5fa19435d86c3fc81bd101f8eade
Vinicius-Maia-A <vinicius.maia2011@gmail.com>
2023-01-09 17:03:10 -0300

    Obtendo um post por id

--- src/main/java/com/viniciusmaia/workshopmongo/repository/UserRepository.java
+++ src/main/java/com/viniciusmaia/workshopmongo/repository/UserRepository.java
@@ -1,7 +1,6 @@
 package com.viniciusmaia.workshopmongo.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
 
 import com.viniciusmaia.workshopmongo.domain.User;
 
